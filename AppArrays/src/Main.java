fa9d7af67c72bf958ec7027d67838d50a1883465
juniorDeveloper8 <be6323787@gmail.com>
2024-08-04 19:01:45 -0500

    agragamos codigo para ejecutar el proyecto

--- AppArrays/src/Main.java
+++ AppArrays/src/Main.java
@@ -22,12 +22,11 @@ public class Main {
 
         //Hacer doble bucle for/for
 
-
         //CLAVE: DESARROLLO
+        System.out.println(config[XPROD][CPASS]); //...
 
         //System.out.println(config[3][1]);
         //System.out.println(config[1][1]);
-        System.out.println(config[XPROD][CPASS]); //...
 
 
     }
